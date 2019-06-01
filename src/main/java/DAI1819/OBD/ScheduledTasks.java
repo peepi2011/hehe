fc10a1a8249b7c6def15c79b16f4af48d105d6a1
José Ribeiro <jose.ribeiro.21598@gmail.com>
2019-06-01 22:52:50 +0100

    Update

--- src/main/java/DAI1819/OBD/ScheduledTasks.java
+++ src/main/java/DAI1819/OBD/ScheduledTasks.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduledTasks {
     
-    @Scheduled(cron = "	0 0/1 * 1/1 * ? *")
+    @Scheduled(cron = "	0 0 0 ? * SUN ")
     public void calculoPerfilRisco(){
     System.out.println("Ola");
     CalculoPerfilRisco calculo = new CalculoPerfilRisco();    
